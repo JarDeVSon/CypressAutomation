b973fe6913c5f546497ab1345ba997ce0d62ebf7
Jardeson Santos <jardounifanor@gmail.com>
2023-05-09 23:17:05 -0300

    upgrade dependencies

--- cypress/pages/HomePage.js
+++ cypress/pages/HomePage.js
@@ -3,8 +3,9 @@ import homepage from '../selectors/Home.sel.cy'
 class HomePage {
     getTextWelcome(){
         console.log(homepage.textWelcome)
-        cy.contains('p','Hello').should('be.visible');
         cy.contains(homepage.textWelcome,'Hello');
+        cy.contains('p','Hello').should('be.visible');
+
     }
 }
 export default HomePage;
