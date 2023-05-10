86746d4876484df7789ce62f71f08e21e8382207
Jardeson Santos <jardounifanor@gmail.com>
2023-05-10 00:53:37 -0300

    Try to viewport tests

--- cypress/e2e/web/MyFirstTest.cy.js
+++ cypress/e2e/web/MyFirstTest.cy.js
@@ -11,35 +11,94 @@ const homePage = new HomePage();
 const faker = require("faker");
 
 describe("My fist test in cypress with pom model", () => {
-  before(() => {
-    // cy.injectAxe();
-    // cy.checkA11y();
-  //   // ;[[1920, 1080], 'ipad-2', 'iphone-xr', 'samsung-note9'].forEach(size => {
-  //   //   if (Cypress._.isArray(size)) {
-  //   //     cy.viewport(size[0], size[1], size[2])
-  //   //   } else {
-  //   //     cy.viewport(size)
-  //   //   }
-  //   // })
-  });
-  it("Registration with successfully", () => {
-    // cy.fixture("mockdata").then((data) => {
-      registrationPage.acessarSite();
-      registrationPage.myAccount();
-      registrationPage.inserirEmail(faker.internet.email());
-      registrationPage.inserirPassword(faker.internet.password());
-      registrationPage.registrar();
-      homePage.getTextWelcome();
-    // });
-  });
-  it("Registration with Cypress Commands with fixture", () => {
-    cy.fixture("mockdata").then((data) => {
-      cy.log(data.email);
-      cy.log(data.password);
-      cy.registration(data.email, data.password);
+    // before(() => {
+  //   // cy.injectAxe();
+  //   // cy.checkA11y();
+  //   ;[[1280, 768], 'ipad-2', 'iphone-xr', 'samsung-note9'].forEach(size => {
+  //     if (Cypress._.isArray(size)) {
+  //       cy.viewport(size[0], size[1], size[2])
+  //     } else {
+  //       cy.viewport(size)
+  //     }
+  //   })
+  // });
+  context('macbook-13 1280,724 resolution', () => {
+    beforeEach(()=>{
+      // cy.viewport('ipad-2');
+      cy.viewport(1280,724);
+    })
+    it("Registration with successfully", () => {
+      // cy.fixture("mockdata").then((data) => {
+        registrationPage.acessarSite();
+        registrationPage.myAccount();
+        registrationPage.inserirEmail(faker.internet.email());
+        registrationPage.inserirPassword(faker.internet.password());
+        registrationPage.registrar();
+        homePage.getTextWelcome();
+      // });
     });
-  });
+  // it("Registration with Cypress Commands with fixture", () => {
+  //   cy.fixture("mockdata").then((data) => {
+  //     cy.log(data.email);
+  //     cy.log(data.password);
+  //     cy.registration(data.email, data.password);
+  //   });
+  // });
   it("Registration with Cypress Commands with faker", () => {
     cy.registration(faker.internet.email(), faker.internet.password());
   });
+  })
+  context('ipad-2 768, 1024', () => {
+    beforeEach(()=>{
+      // cy.viewport('ipad-2');
+      cy.viewport(768,1024);
+    })
+    it("Registration with successfully", () => {
+      // cy.fixture("mockdata").then((data) => {
+        registrationPage.acessarSite();
+        registrationPage.myAccount();
+        registrationPage.inserirEmail(faker.internet.email());
+        registrationPage.inserirPassword(faker.internet.password());
+        registrationPage.registrar();
+        homePage.getTextWelcome();
+      // });
+    });
+  // it("Registration with Cypress Commands with fixture", () => {
+  //   cy.fixture("mockdata").then((data) => {
+  //     cy.log(data.email);
+  //     cy.log(data.password);
+  //     cy.registration(data.email, data.password);
+  //   });
+  // });
+  it("Registration with Cypress Commands with faker", () => {
+    cy.registration(faker.internet.email(), faker.internet.password());
+  });
+  })
+  context('iphone-5 320, 480 resolution', () => {
+    beforeEach(()=>{
+      // cy.viewport('iphone-5');
+      cy.viewport(320,480);
+
+    })
+    it("Registration with successfully", () => {
+      // cy.fixture("mockdata").then((data) => {
+        registrationPage.acessarSite();
+        registrationPage.myAccount();
+        registrationPage.inserirEmail(faker.internet.email());
+        registrationPage.inserirPassword(faker.internet.password());
+        registrationPage.registrar();
+        homePage.getTextWelcome();
+      // });
+    });
+  // it("Registration with Cypress Commands with fixture", () => {
+  //   cy.fixture("mockdata").then((data) => {
+  //     cy.log(data.email);
+  //     cy.log(data.password);
+  //     cy.registration(data.email, data.password);
+  //   });
+  // });
+  it("Registration with Cypress Commands with faker", () => {
+    cy.registration(faker.internet.email(), faker.internet.password());
+  });
+  })
 });
